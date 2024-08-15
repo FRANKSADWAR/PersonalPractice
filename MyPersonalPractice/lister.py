b2b74f80a980a37b859889255b9593e6ab150f63
FRANKSADWAR <billyfranks98@gmail.com>
2024-08-15 06:29:11 +0300

    list tree

--- MyPersonalPractice/lister.py
+++ MyPersonalPractice/lister.py
@@ -57,13 +57,67 @@ class ListInherited:
         return result
 
 
+class ListTree:
+    """
+    Mixin that returns an __str__ trace of the entire class tree and all its objects's attrs at and above self;
+    Run by print, str() returns constructed string
+    Uses __X attr names to avoid impacting clients
+    Uses generator expression to recurse to the superclasses
+    Uses str.format() to make substitutions clearer
+
+    """
+
+    def __str__(self):
+        self.__visited = {}
+        return '< Instance of {0}, address {1} : \n{2} {3}>'.format(
+            self.__class__.__name__,
+            id(self),
+            self.__attrnames(self,0),
+            self.__listclass(self.__class__,4)
+
+        )
+
+
+    def __listclass(self, aClass, indent):
+        dots = '.' * indent
+        if aClass in self.__visited:
+            return '\n{0} <Class {1}:, address {2}: (see above)>'.format(
+                dots,
+                aClass.__name__,
+                id(aClass)
+            )
+        
+        else:
+            self.__visited[aClass] = True
+            genabove = (self.__listclass(c, indent+4) for c in aClass.__bases__)
+            return '\n<Class {1}, address {2}: \n {3}{4}{5}>\n'.format(
+                dots,
+                aClass.__name__,
+                id(aClass),
+                self.__attrnames(aClass, indent),
+                ''.join(genabove),
+                dots
+            )
+        
+
+    def __attrnames(self, obj, indent):
+        spaces = ' ' * (indent + 4)
+        result = ''
+        for attr in sorted(obj.__dict__):
+            if attr.startswith('__') and attr.endswith('__'):
+                result += spaces + '{0}=<>\n'.format(attr)
+            else:
+                result += spaces + '{0} = {1}\n'.format(attr, getattr(obj, attr))
+        return result
+
+
 class Super:
     def __init__(self):
         self.data1 = 'Spam'
     def ham(self):
         pass
 
-class Sub(Super, ListInherited):
+class Sub(Super, ListTree):
     def __init__(self):
         Super.__init__(self)
         self.data2 = 'Eggs'   # instance attributes
