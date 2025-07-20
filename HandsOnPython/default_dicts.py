a2fc14d1b3d6a3be168fd364253d4f647761ab8c
FRANKSADWAR <billyfranks98@gmail.com>
2025-07-20 21:37:32 +0300

    reminders

--- HandsOnPython/default_dicts.py
+++ HandsOnPython/default_dicts.py
@@ -1,5 +1,8 @@
 from collections import defaultdict
+import string
+from datetime import datetime
 
+### DEFAULTDICT
 """
 Defaultdict is a subclass of the dict built in object from the collections module provides
 a default value for a nonexistent key so that no KeyError is raised when trying to access 
@@ -33,3 +36,14 @@ for letter in "banana":
 print(dict(counts))
 
 help(defaultdict)
+
+
+## Module: Is a single Python file, anyone can create a module apart from the built-in modules
+
+## Packages: A collection of modules (library), most are publicly available and free
+
+## PIP: Preferred Installer Program is a tool used to install Python Packages
+
+## Function is code to perform a task i.e sum, len function
+
+## Method a function that is specific to a data type or an object i.e df.head() or df.groupby()
